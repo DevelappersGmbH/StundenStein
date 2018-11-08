8960d46f5ef25ff0684d5de3ee191db3e910c712
Jakob Neumann <mail@jakobneumann.com>
2018-11-08 12:18:56 +0100

    Auto-change project selection when selecting issue

--- src/app/components/time-tracker/time-tracker.component.ts
+++ src/app/components/time-tracker/time-tracker.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RedmineService } from 'src/app/services/redmine/redmine.service';
 import { Project } from 'src/app/model/project.interface';
 import { Issue } from 'src/app/model/issue.interface';
+import { isUndefined } from 'util';
 
 @Component({
   selector: 'app-time-tracker',
@@ -14,15 +15,45 @@ export class TimeTrackerComponent implements OnInit {
 
   projects: Project[];
   issues: Issue[];
+  selectedIssue: Issue;
+  selectedProject: Project;
 
   ngOnInit() {
     this.loadProjects();
     this.loadIssues();
   }
 
+  selectIssue() {
+    if (isUndefined(this.selectedIssue)) {
+      this.selectedProject = undefined;
+    } else {
+      this.selectedProject = this.selectedIssue.project;
+      if (!this.projects.includes(this.selectedProject)) {
+        this.projects.forEach( project => {
+          if (JSON.stringify(project) === JSON.stringify(this.selectedProject)) {
+            this.selectedProject = project;
+          }
+        });
+      }
+    }
+    console.log(this.selectedProject);
+  }
+
+  selectProject() {
+    console.log(this.selectedProject);
+  }
+
   loadProjects() {
     this.redmineService.getProjects().subscribe(data => {
-      this.projects = data.projects;
+      this.projects = new Array();
+      data.projects.forEach( project => {
+        this.projects.push(
+          {
+            id: project.id,
+            name: project.name
+          }
+        );
+      });
     }, error => {
       console.error(error);
     });
