ca13cb9c58f07183107b581a94accb21f9331569
Jakob Neumann <mail@jakobneumann.com>
2018-11-08 11:29:57 +0100

    Add issueGetter to redmine service

--- src/app/services/redmine/redmine.service.ts
+++ src/app/services/redmine/redmine.service.ts
@@ -4,12 +4,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RedmineProjects } from 'src/app/redmine-model/redmine-projects.interface';
+import { RedmineIssue } from 'src/app/redmine-model/redmine-issue.interface';
+import { RedmineIssuesRequest } from 'src/app/redmine-model/requests/redmine-issues-request.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RedmineService extends BaseDataService {
   private projectsUrl = '/projects';
+  private issuesUrl = '/issues';
 
   constructor(
     protected authenticationService: AuthenticationService,
@@ -22,5 +25,11 @@ export class RedmineService extends BaseDataService {
     const endpoint = this.getJsonEndpointUrl(this.projectsUrl);
     console.log(endpoint);
     return this.httpClient.get<RedmineProjects>(endpoint);
+  }
+
+  getIssues(): Observable<RedmineIssuesRequest> {
+    const endpoint = this.getJsonEndpointUrl(this.issuesUrl);
+    console.log(endpoint);
+    return this.httpClient.get<RedmineIssuesRequest>(endpoint);
   }
 }
