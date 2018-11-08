8e07f937e896d91517fa838540af713efe9a48e4
Jakob Neumann <mail@jakobneumann.com>
2018-11-08 11:30:32 +0100

    Get issues in tracker component

--- src/app/components/time-tracker/time-tracker.component.ts
+++ src/app/components/time-tracker/time-tracker.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RedmineService } from 'src/app/services/redmine/redmine.service';
 import { Project } from 'src/app/model/project.interface';
+import { Issue } from 'src/app/model/issue.interface';
 
 @Component({
   selector: 'app-time-tracker',
@@ -12,14 +13,35 @@ export class TimeTrackerComponent implements OnInit {
   constructor( private redmineService: RedmineService ) { }
 
   projects: Project[];
+  issues: Issue[];
 
   ngOnInit() {
     this.loadProjects();
+    this.loadIssues();
   }
 
   loadProjects() {
     this.redmineService.getProjects().subscribe(data => {
       this.projects = data.projects;
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  loadIssues() {
+    this.redmineService.getIssues().subscribe(data => {
+      this.issues = new Array();
+      data.issues.forEach( issue => {
+        this.issues.push(
+          {
+            id: issue.id,
+            subject: issue.subject,
+            project: issue.project,
+            tracker: issue.tracker.name,
+            assignedTo: issue.assigned_to
+          }
+        );
+      });
     }, error => {
       console.error(error);
     });
