797cf0cb5938cc693bcd711794f8a6adc244a02b
Felix Guttbier <felix.guttbier@mailbox.tu-dresden.de>
2018-11-10 12:55:38 +0100

    Updated RedmineService to load more items in getProjects and in getIssues

--- src/app/services/redmine/redmine.service.ts
+++ src/app/services/redmine/redmine.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RedmineIssues } from 'src/app/redmine-model/redmine-issue.interface';
+import { RedmineProject } from 'src/app/redmine-model/redmine-project.interface';
 import { RedmineProjects } from 'src/app/redmine-model/redmine-projects.interface';
 
 @Injectable({
@@ -21,14 +22,12 @@ export class RedmineService extends BaseDataService {
   }
 
   getProjects(): Observable<RedmineProjects> {
-    const endpoint = this.getJsonEndpointUrl(this.projectsUrl);
-    console.log(endpoint);
+    const endpoint = this.getJsonEndpointUrl(this.projectsUrl) + '?limit=100';
     return this.httpClient.get<RedmineProjects>(endpoint);
   }
 
   getIssues(): Observable<RedmineIssues> {
-    const endpoint = this.getJsonEndpointUrl(this.issuesUrl);
-    console.log(endpoint);
+    const endpoint = this.getJsonEndpointUrl(this.issuesUrl) + '?limit=100';
     return this.httpClient.get<RedmineIssues>(endpoint);
   }
 }
