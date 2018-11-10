aa1b10dfe15a3fa1815843b6fe8bc12831a667e2
Felix Guttbier <felix.guttbier@mailbox.tu-dresden.de>
2018-11-10 14:54:53 +0100

    Updated RedmineProjects with attributes needed

--- src/app/redmine-model/redmine-projects.interface.ts
+++ src/app/redmine-model/redmine-projects.interface.ts
@@ -2,4 +2,7 @@ import { RedmineProject } from './redmine-project.interface';
 
 export interface RedmineProjects {
   projects: RedmineProject[];
+  total_count: number;
+  offset: number;
+  limit: number;
 }
