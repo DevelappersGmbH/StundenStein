385f414f1fdded0504d63f20cd33fc87d766fd09
Felix Guttbier <felix.guttbier@mailbox.tu-dresden.de>
2018-11-10 12:56:41 +0100

    Updated DataService: getIssues is functional now. Added experimental code for caching projects and issues.

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,10 +1,16 @@
+import { forkJoin, Observable } from 'rxjs';
 import { HourGlassService } from '../hourglass/hourglass.service';
 import { Injectable } from '@angular/core';
 import { Issue } from 'src/app/model/issue.interface';
-import { map, mapTo, switchMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import {
+  map,
+  mapTo,
+  switchMap,
+  tap
+  } from 'rxjs/operators';
 import { Project } from 'src/app/model/project.interface';
 import { RedmineIssues } from 'src/app/redmine-model/redmine-issue.interface';
+import { RedmineProject } from 'src/app/redmine-model/redmine-project.interface';
 import { RedmineProjects } from 'src/app/redmine-model/redmine-projects.interface';
 import { RedmineService } from '../redmine/redmine.service';
 
@@ -12,15 +18,30 @@ import { RedmineService } from '../redmine/redmine.service';
   providedIn: 'root'
 })
 export class DataService {
+  private cachedProjects: Project[];
+  private cachedIssues: Partial<Issue>[];
+
   constructor(
     private redmineService: RedmineService,
     private hourglassService: HourGlassService
-  ) {}
+  ) {
+    this.cachedProjects = [];
+    this.cachedIssues = [];
+  }
 
   getProjects(): Observable<Project[]> {
     return this.redmineService
       .getProjects()
-      .pipe(map(p => this.mapRedmineProjectsToProjectArray(p)));
+      .pipe(map(p => this.mapRedmineProjectsToProjectArrayAndStore(p)));
+  }
+
+  mapRedmineProjectsToProjectArrayAndStore(
+    redmineProjects: RedmineProjects
+  ): Project[] {
+    this.cachedProjects = this.mapRedmineProjectsToProjectArray(
+      redmineProjects
+    );
+    return this.cachedProjects;
   }
 
   mapRedmineProjectsToProjectArray(
@@ -28,22 +49,46 @@ export class DataService {
   ): Project[] {
     const projects: Project[] = [];
     redmineProjects.projects.forEach(project => {
-      projects.push({
-        id: project.id,
-        name: project.name
-      });
+      projects.push(this.mapRedmineProjectToProject(project));
     });
     return projects;
   }
 
-  getIssues(): Observable<Partial<Issue>[]> {
-    return this.redmineService
-      .getIssues()
-      .pipe(map(p => this.mapRedmineIssuesToIssueArray(p)));
+  mapRedmineProjectToProject(redmineProject: RedmineProject): Project {
+    return { id: redmineProject.id, name: redmineProject.name };
   }
 
-  mapRedmineIssuesToIssueArray(redmineIssues: RedmineIssues): Partial<Issue>[] {
-    const issues: Partial<Issue>[] = [];
+  getIssues(): Observable<Partial<Issue>[]> {
+    const calls: Observable<any>[] = [];
+    if (!this.cachedProjects || this.cachedProjects.length === 0) {
+      calls.push(this.redmineService.getProjects());
+    }
+    calls.push(this.redmineService.getIssues());
+    return forkJoin(calls).pipe(
+      map(results => this.mapRedmineIssuesToIssueArray(results))
+    );
+  }
+
+  mapRedmineIssuesToIssueArray(results: any[]): Partial<Issue>[] {
+    let redmineIssues: RedmineIssues;
+    if (!results || results.length === 0) {
+      console.error(
+        'IssuesConversion: No input parameters, Array is empty or null'
+      );
+    }
+    if (results.length === 1 && !(<RedmineIssues>results[0]).issues) {
+      console.error(
+        'IssuesConversion the only parameter in Array is not RedmineIssues!'
+      );
+    } else {
+      redmineIssues = results[1];
+    }
+    if (results.length === 2 && (<RedmineProjects>results[0]).projects) {
+      this.cachedProjects = this.mapRedmineProjectsToProjectArray(results[0]);
+      redmineIssues = results[1];
+    }
+
+    this.cachedIssues = [];
     redmineIssues.issues.forEach(redmineIssue => {
       const issue: Partial<Issue> = {
         id: redmineIssue.id,
@@ -52,8 +97,16 @@ export class DataService {
       if (redmineIssue.assigned_to) {
         issue.assignedTo = redmineIssue.assigned_to;
       }
-      issues.push(issue);
+      if (redmineIssue.tracker) {
+        issue.tracker = redmineIssue.tracker.name;
+      }
+      if (redmineIssue.project) {
+        issue.project = this.cachedProjects.find(
+          p => p.id === redmineIssue.project.id
+        );
+      }
+      this.cachedIssues.push(issue);
     });
-    return issues;
+    return this.cachedIssues;
   }
 }
