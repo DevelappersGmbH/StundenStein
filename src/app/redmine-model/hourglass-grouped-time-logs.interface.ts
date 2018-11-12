fd4d4e164af78b86a093cbe4d666afbdf39a83d8
Felix Guttbier <felix.guttbier@mailbox.tu-dresden.de>
2018-11-12 21:20:52 +0100

    Removed unneeded model

--- src/app/redmine-model/hourglass-grouped-time-logs.interface.ts
+++
@@ -1,7 +0,0 @@
-import { HourGlassTimeTracker } from './hourglass-time-tracker.interface';
-
-export interface HourGlassGroupedTimeLogs {
-  time_sum: number;
-  int_date: number;
-  time_logs: HourGlassTimeTracker[];
-}
