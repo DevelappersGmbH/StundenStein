a88e62faf43cc95d8c838d11325c5f3d8b6d6b57
Jakob Neumann <mail@jakobneumann.com>
2018-11-15 20:13:45 +0100

    Some minor improvements, remove console.log

--- src/app/components/recent-time-logs/recent-time-logs.component.ts
+++ src/app/components/recent-time-logs/recent-time-logs.component.ts
@@ -3,13 +3,14 @@ import { TimeLog } from 'src/app/model/time-log.interface';
 import { UserService } from 'src/app/services/user/user.service';
 import { Issue } from 'src/app/model/issue.interface';
 import { Project } from 'src/app/model/project.interface';
+import { User } from 'src/app/model/user.interface';
 
 @Component({
   selector: 'app-recent-time-logs',
   templateUrl: './recent-time-logs.component.html',
   styleUrls: ['./recent-time-logs.component.scss']
 })
- 
+
 export class RecentTimeLogsComponent implements OnInit {
 
   constructor() {}
@@ -20,46 +21,44 @@ export class RecentTimeLogsComponent implements OnInit {
     this.loadTimeLogs();
   }
 
-  clickedItem(){
-    console.log("clicked item");
-  }
+  clickedItem() { }
 
-  loadTimeLogs(){
+  loadTimeLogs() {
     this.timeLogList = this.createTimeLogsArray(1000);
   }
 
-  createTimeLogsArray(numberOfTimeLogs){
-    let timeLogList = [];
-    var project = {
+  createTimeLogsArray(numberOfTimeLogs) {
+    const timeLogList = [];
+    const project: Project = {
       id: 1,
-      name: "prototypeProject"
+      name: 'prototypeProject'
     };
 
-    var user = {
+    const user: User = {
       id: 1,
-      name: "prototypeUser"
+      name: 'prototypeUser'
     };
 
-    var issue = {
+    const issue: Issue = {
       id: 1,
-      subject: "prototypeSubject",
-      tracker: "prototypeTracker",
+      subject: 'prototypeSubject',
+      tracker: 'prototypeTracker',
       project: project,
       assignedTo: user
     };
-    for(let i=0;i<numberOfTimeLogs;i++){
-      var timeLog = {
-      id: i,
-      timeStarted: "10AM",
-      timeStopped: "12 PM",
-      comment: "",
-      timeInHours: 14,
-      booked: true,
-      hourGlassTimeBookingId: i,
-      billable: false,
-      issue: issue,
-      project: project,
-      user: user
+    for (let i = 0; i < numberOfTimeLogs; i++) {
+      const timeLog: TimeLog = {
+        id: i,
+        timeStarted: '10 AM',
+        timeStopped: '12 PM',
+        comment: '',
+        timeInHours: 14,
+        booked: true,
+        hourGlassTimeBookingId: i,
+        billable: false,
+        issue: issue,
+        project: project,
+        user: user
       };
       timeLogList.push(timeLog);
     }
