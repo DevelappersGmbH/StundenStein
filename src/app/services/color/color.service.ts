f6cfbe64a976d039f4a77926f3f6005305d4c4ca
Jakob Neumann <mail@jakobneumann.com>
2018-11-17 18:07:19 +0100

    ISSUE-47: Remove yellow color point, adjust some other parameters

--- src/app/services/color/color.service.ts
+++ src/app/services/color/color.service.ts
@@ -35,15 +35,15 @@ export class ColorService {
    */
   private getHSL(hash: number): string {
     // const hue = Math.abs(hash % 360);
-    const hue = this.getHue(Math.abs(hash % 7), hash % 15);
+    const hue = this.getHue(Math.abs(hash % 6), hash % 15);
     const saturation = 80 + Math.abs(((hash + 1) % 20));
-    const lightness = 40 + Math.abs(((hash + 2) % 20));
+    const lightness = 40 + Math.abs(((hash + 2) % 15));
     return 'hsl(' + hue + ',' + saturation + '%,' + lightness + '%)';
   }
 
   /**
    * Gets hue value by selecting a specific point in the hue and adding some randomization
-   * @param selector unsigned integer between 0 and 6
+   * @param selector unsigned integer between 0 and 5
    * @param randomizer signed integer (f.e. between -10 and 10)
    */
   private getHue(selector: number, randomizer: number): number {
@@ -51,10 +51,9 @@ export class ColorService {
     let hue = 300; // pinkish color
     if (selector === 0) { hue = 0; } // red
     if (selector === 1) { hue = 30; } // orange
-    if (selector === 2) { hue = 60; } // yellow
-    if (selector === 3) { hue = 120; } // green
-    if (selector === 4) { hue = 180; } // turqoise
-    if (selector === 5) { hue = 240; } // blue
+    if (selector === 2) { hue = 120; } // green
+    if (selector === 3) { hue = 180; } // turquoise
+    if (selector === 4) { hue = 240; } // blue
     hue += randomizer;
     if (hue < 0)   { hue = 360 - hue; }
     if (hue > 360) { hue = hue - 360; }
