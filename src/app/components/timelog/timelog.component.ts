41122541917b53f3014ea16b7bdd8da724438584
hlnstepanova <hlnstepanova@yandex.ru>
2018-11-20 10:05:03 +0100

    Add default for project name and issue subject if project or issue undefined

--- src/app/components/timelog/timelog.component.ts
+++ src/app/components/timelog/timelog.component.ts
@@ -44,8 +44,8 @@ export class TimeLogComponent implements OnInit {
   filteredProjectOptions: Observable<string[]>;
 
   ngOnInit() {
-    this.currentIssueSubject = this.timeLog.issue.subject;
-    this.currentProjectName = this.timeLog.project.name;
+    this.currentIssueSubject = this.timeLog.issue ? this.timeLog.issue.subject : '';
+    this.currentProjectName = this.timeLog.project ? this.timeLog.project.name : '';
     this.currentIssue = this.timeLog.issue;
     this.currentProject = this.timeLog.project;
     this.currentComment = this.timeLog.comment;
