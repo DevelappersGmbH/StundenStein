4e33255883a0352a65905d1bc2a2fa791c0f9ef8
YannickSpoerl <spoerlyannick@gmail.com>
2018-11-27 14:02:06 +0100

    Begin testing

--- src/app/directives/enforced-inputs.directive.spec.ts
+++ src/app/directives/enforced-inputs.directive.spec.ts
@@ -4,7 +4,7 @@ import { EnforcedInputsDirective } from './enforced-inputs.directive';
 describe('EnforcedInputsDirective', () => {
   it('should create an instance', () => {
     // DANGER DANGER
-    const directive = new EnforcedInputsDirective(null, null);
+    const directive = new EnforcedInputsDirective(undefined, undefined);
     expect(directive).toBeTruthy();
   });
 });
