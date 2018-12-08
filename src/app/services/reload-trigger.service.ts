27b69432b75d6cf5270bd34665751f6c9f2770cd
Felix Guttbier <felix.guttbier@mailbox.tu-dresden.de>
2018-12-08 15:55:53 +0100

    Implemented TimeLogAdded in ReloadTriggerService

--- src/app/services/reload-trigger.service.ts
+++ src/app/services/reload-trigger.service.ts
@@ -1,15 +1,22 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TimeLog } from '../model/time-log.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReloadTriggerService {
+  public timeLogAdded: EventEmitter<TimeLog>;
   public timeLogDeleted: EventEmitter<number>;
   public timeLogUpdated: EventEmitter<number>;
 
   constructor() {
+    this.timeLogAdded = new EventEmitter();
     this.timeLogDeleted = new EventEmitter();
     this.timeLogUpdated = new EventEmitter();
+  }
+
+  public triggerTimeLogAdded(timelog: TimeLog) {
+    this.timeLogAdded.emit(timelog);
   }
 
   public triggerTimeLogDeleted(id: number = -1) {
