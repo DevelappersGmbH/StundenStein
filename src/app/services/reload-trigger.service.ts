c79ccea9bb8f0f127e5f0ec456b15a77cb546f50
Felix Guttbier <felix.guttbier@mailbox.tu-dresden.de>
2018-12-08 15:53:50 +0100

    Implemented TimeLogUpdated in ReloadTriggerService

--- src/app/services/reload-trigger.service.ts
+++ src/app/services/reload-trigger.service.ts
@@ -5,12 +5,18 @@ import { EventEmitter, Injectable } from '@angular/core';
 })
 export class ReloadTriggerService {
   public timeLogDeleted: EventEmitter<number>;
+  public timeLogUpdated: EventEmitter<number>;
 
   constructor() {
     this.timeLogDeleted = new EventEmitter();
+    this.timeLogUpdated = new EventEmitter();
   }
 
   public triggerTimeLogDeleted(id: number = -1) {
     this.timeLogDeleted.emit(id);
+  }
+
+  public triggerTimeLogUpdated(id: number = -1) {
+    this.timeLogUpdated.emit(id);
   }
 }
