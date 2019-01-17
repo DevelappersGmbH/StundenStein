4ff0d3e1788b060093d33b382040699ce96e3257
YannickSpoerl <spoerlyannick@gmail.com>
2019-01-17 15:52:45 +0100

    Comment and cleanup RecentTimeLogsComponent

--- src/app/components/recent-time-logs/recent-time-logs.component.ts
+++ src/app/components/recent-time-logs/recent-time-logs.component.ts
@@ -5,11 +5,9 @@ import {
   OnInit,
   SimpleChanges
   } from '@angular/core';
-import { DataService } from 'src/app/services/data/data.service';
 import { Issue } from 'src/app/model/issue.interface';
 import { Project } from './../../model/project.interface';
 import { TimeLog } from 'src/app/model/time-log.interface';
-import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-recent-time-logs',
@@ -17,22 +15,19 @@ import { UserService } from 'src/app/services/user/user.service';
   styleUrls: ['./recent-time-logs.component.scss']
 })
 export class RecentTimeLogsComponent implements OnInit, OnChanges {
-  constructor(
-    private dataService: DataService,
-    private userService: UserService
-  ) {
+  constructor() {
     this.listLoading = true;
     this.numberOfUnbookedTimeLogs = 0;
     this.timeLogMap = new Map();
     this.unbookedTimeLogsMap = new Map();
   }
 
-  timeLogObservablesList: TimeLog[];
-  dateList: Date[];
-  timeLogMap: Map<Date, TimeLog[]>;
-  unbookedTimeLogsMap: Map<Date, number>;
-  numberOfUnbookedTimeLogs: number;
-  listLoading: boolean;
+  timeLogList: TimeLog[]; // holds all timeLogs
+  dateList: Date[]; // holds all dates (matcard for every date)
+  timeLogMap: Map<Date, TimeLog[]>; // corresponding timeLog-array for each date (matlist for every date)
+  unbookedTimeLogsMap: Map<Date, number>; // maps number of unbooked timeLogs to each date (for Badge on each date)
+  numberOfUnbookedTimeLogs: number; // for Badge at headline
+  listLoading: boolean; // loading spinner
 
   @Input() projects: Project[];
   @Input() issues: Issue[];
@@ -40,39 +35,45 @@ export class RecentTimeLogsComponent implements OnInit, OnChanges {
 
   ngOnInit() {}
 
+  // recalculates entire List with the new TimeLogs
   ngOnChanges(changes: SimpleChanges) {
     this.listLoading = true;
     if (typeof changes['timeLogs'] !== 'undefined') {
       const change = changes['timeLogs'];
-      this.timeLogObservablesList = change.currentValue;
+      this.timeLogList = change.currentValue;
       this.seperateDates();
       this.countUnbookedTimeLogs();
       this.listLoading = false;
     }
   }
 
+  // creates array with all existing dates, maps arrya of corresponding timeLogs to each date
   seperateDates() {
     this.timeLogMap = new Map();
     const seperateDates: Date[] = new Array();
-    this.timeLogObservablesList.forEach(timeLog => {
+    this.timeLogList.forEach(timeLog => {
       let dateExists: Boolean = false;
       let newDate: Date;
+      // check if date already exists in map
       seperateDates.forEach(date => {
         if (this.compareDatesEqual(timeLog.timeStarted, date)) {
           dateExists = true;
           newDate = date;
         }
       });
+      // create new array and insert it at the date
       if (!dateExists) {
         newDate = timeLog.timeStarted;
         seperateDates.push(newDate);
         this.timeLogMap.set(newDate, new Array());
       }
+      // put the timeLog to the list corresponding to its date
       this.timeLogMap.get(newDate).push(timeLog);
     });
-    this.dateList = seperateDates;
+    this.dateList = seperateDates; // create the new dateList
   }
 
+  // compare two dates, ignoring hours, minutes and seconds
   compareDatesEqual(d1: Date, d2: Date) {
     if (
       d1.getUTCDate() === d2.getUTCDate() &&
@@ -84,6 +85,7 @@ export class RecentTimeLogsComponent implements OnInit, OnChanges {
     return false;
   }
 
+  // maps number of unbooked timeLogs to each date
   countUnbookedTimeLogs() {
     this.numberOfUnbookedTimeLogs = 0;
     this.timeLogMap.forEach((timeLogs: TimeLog[], date: Date) => {
