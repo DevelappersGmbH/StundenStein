d3604de3ae4f55c4db0227a2534cb70002e26227
Jakob Neumann <mail@jakobneumann.com>
2019-01-18 16:24:54 +0100

    Add some imports

--- src/app/components/time-tracker/time-tracker.component.spec.ts
+++ src/app/components/time-tracker/time-tracker.component.spec.ts
@@ -1,6 +1,6 @@
 import { UserServiceMock } from './../../services/mocked-services/UserServiceMock.spec';
 import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material';
+import { MatFormFieldModule, MatAutocompleteModule } from '@angular/material';
 import { DataService } from 'src/app/services/data/data.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
@@ -8,6 +8,7 @@ import { TimeTrackerComponent } from './time-tracker.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { UserService } from 'src/app/services/user/user.service';
 import { DataServiceMock } from 'src/app/services/mocked-services/DataServiceMock.spec';
+import { Favicons } from 'src/app/services/favicon/favicon.service';
 
 describe('TimeTrackerComponent', () => {
   let component: TimeTrackerComponent;
@@ -15,9 +16,10 @@ describe('TimeTrackerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [],
+      imports: [ MatAutocompleteModule ],
       declarations: [ TimeTrackerComponent ],
-      providers: [{provide : DataService, useClass: DataServiceMock}, {provide: UserService, useClass: UserServiceMock}],
+      providers: [{provide : DataService, useClass: DataServiceMock}, {provide: UserService, useClass: UserServiceMock},
+        {provide: Favicons, useClass: Favicons}],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
