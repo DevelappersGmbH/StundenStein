ae5c1246011cf586721044765f45251e8b076bdf
YannickSpoerl <spoerlyannick@gmail.com>
2019-01-18 13:31:16 +0100

    Fix wrong imports

--- src/app/directives/enforced-inputs.directive.spec.ts
+++ src/app/directives/enforced-inputs.directive.spec.ts
@@ -2,7 +2,7 @@ import { EnforcedInputsDirective } from './enforced-inputs.directive';
 
 describe('EnforcedInputsDirective', () => {
   it('should create an instance', () => {
-    const directive = new EnforcedInputsDirective();
+    const directive = new EnforcedInputsDirective(undefined, undefined);
     expect(directive).toBeTruthy();
   });
 });
--- src/app/services/authguard/authguard.service.spec.ts
+++ src/app/services/authguard/authguard.service.spec.ts
@@ -1,12 +1,11 @@
+import { AuthGuard } from './authguard.service';
 import { TestBed } from '@angular/core/testing';
-
-import { AuthguardService } from './authguard.service';
 
 describe('AuthguardService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: AuthguardService = TestBed.get(AuthguardService);
+    const service: AuthGuard = TestBed.get(AuthGuard);
     expect(service).toBeTruthy();
   });
 });
