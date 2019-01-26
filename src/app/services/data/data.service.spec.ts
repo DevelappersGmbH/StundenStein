d82abc7404454af3697fdf36b8f45eb430b86208
YannickSpoerl <spoerlyannick@gmail.com>
2019-01-26 17:14:51 +0100

    Fix imports

--- src/app/services/data/data.service.spec.ts
+++ src/app/services/data/data.service.spec.ts
@@ -1,9 +1,17 @@
+import { CookieService } from 'ngx-cookie-service';
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 
 describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [CookieService, DatePipe]
+})
+  );
 
   it('should be created', () => {
     const service: DataService = TestBed.get(DataService);
